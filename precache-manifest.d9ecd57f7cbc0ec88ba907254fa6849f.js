989d10cfdff90985ed155d371806fa106fd37fd0
divyanshudixit <divyanshudixit11@gmail.com>
2022-06-27 19:36:43 +0530

    Updates

---
+++ precache-manifest.d9ecd57f7cbc0ec88ba907254fa6849f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ac74b9d7ed8f08950e",
+    "url": "/movies/static/css/main.c2881c1e.chunk.css"
+  },
+  {
+    "revision": "60ac74b9d7ed8f08950e",
+    "url": "/movies/static/js/main.60ac74b9.chunk.js"
+  },
+  {
+    "revision": "4e9aafb99959cf9fe670",
+    "url": "/movies/static/js/1.4e9aafb9.chunk.js"
+  },
+  {
+    "revision": "2364398bdd5358cd286d",
+    "url": "/movies/static/js/2.2364398b.chunk.js"
+  },
+  {
+    "revision": "4d51b00b6602ae70b9b2",
+    "url": "/movies/static/js/runtime~main.4d51b00b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/movies/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9bd1b943477cc274385778d10f54b2a0",
+    "url": "/movies/index.html"
+  }
+];
